d6da988f0c1c1937721b59db5cbe67b2577964c9
Vladimir Kokovic <vlada.kokovic@gmail.com>
2024-02-28 17:58:49 +0100

    Fix dependencies, bump version to 0.1.2 (#10)

---
+++ dist/src/components/Mark.js
@@ -0,0 +1,10 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import React, { useCallback } from 'react';
+const Mark = (props) => {
+    const { category, start, end, content, onClick, markerClassName } = props;
+    const handleClick = useCallback(() => {
+        onClick({ start, end });
+    }, [onClick, start, end]);
+    return (_jsx("mark", { className: markerClassName, style: { backgroundColor: category?.color || '#84d2ff' }, "data-start": start, "data-end": end, onClick: handleClick, children: content }));
+};
+export default React.memo(Mark);
